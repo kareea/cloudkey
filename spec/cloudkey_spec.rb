6b183236dcb70c50fa80d1bb89b77a8137525e72
JH. Chabran <jh@chabran.fr>
2010-10-20 17:00:33 +0200

    Adding new API specifications

--- spec/cloudkey_spec.rb
+++ spec/cloudkey_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe Cloudkey do
-  it "should do something" do
-    true.should be_true
+  context "Public API" do
+    it "should provide an authenticate method on Cloudkey" do
+      Cloudkey.should respond_to :authenticate
+    end
+    
+    it "should count medias" do
+      pending
+      @cloudkey = Cloudkey.authenticate "foo", "bar"
+      @cloudkey.media.count
+    end
   end
 end

8cf6365c2b2b668e52cd3ba08a3e5a23d4c842d5
JH. Chabran <jh@chabran.fr>
2010-10-20 17:27:59 +0200

    Add more specs for public API

--- spec/cloudkey_spec.rb
+++ spec/cloudkey_spec.rb
@@ -11,5 +11,15 @@ describe Cloudkey do
       @cloudkey = Cloudkey.authenticate "foo", "bar"
       @cloudkey.media.count
     end
+    
+    it "should accept an optional proxy" do
+      @proxy = "http://my.awesome.proxy.com:3128"
+      Cloudkey.authenticate("foo", "bar", :proxy => @proxy).proxy.should == @proxy
+    end
+    
+    it "should accept an optional base_url" do
+      @base_url = "http://different.api.dmcloud.net"
+      Cloudkey.authenticate("foo", "bar", :base_url => @base_url).base_url.should == @base_url
+    end
   end
 end

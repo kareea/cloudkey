111190a2d77a0361da79cdaafb4dec7292a8b493
JH. Chabran <jh@chabran.fr>
2010-10-27 20:34:03 +0200

    Add file upload support

--- lib/cloudkey/file.rb
+++ lib/cloudkey/file.rb
@@ -1,5 +1,13 @@
 module Cloudkey
   class File < Client
+    def upload_file path
+      raise "File not found" unless File.exists? path
+      
+      @curl = curl   
+      @curl.multipart_form_post
     
+      @curl.http_post Curl::PostField.file(path)
+      JSON.parse curl.body_str
+    end
   end
 end

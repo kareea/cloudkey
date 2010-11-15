a19ac4f42a94b5dd9838a9d666419301ccedc041
Jean-Hadrien Chabran <jh@chabran.fr>
2010-11-15 11:15:28 +0100

    Fix a typo

--- lib/cloudkey/media.rb
+++ lib/cloudkey/media.rb
@@ -6,7 +6,7 @@ module Cloudkey
     end
     
     def stream_url id, asset_name="mp4_h264_aac", security_level=SecurityPolicy.new, cdn_url='http://cdn.dmcloud.net'
-      url = "#{cdn_url}/route/#{@api.user_id}/#{id}/#{asset_name}.#{asset_name.split.('_')[0]}"
+      url = "#{cdn_url}/route/#{@api.user_id}/#{id}/#{asset_name}.#{asset_name.split('_')[0]}"
       API.sign_url url, @api.key, security_level
     end
   end

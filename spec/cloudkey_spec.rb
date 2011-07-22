ff86f6731046aba977c930ee00d01374721f9f15
Jean-Hadrien Chabran <jh@chabran.fr>
2011-07-22 14:57:18 +0200

    Add VCR

--- spec/cloudkey_spec.rb
+++ spec/cloudkey_spec.rb
@@ -5,18 +5,19 @@ describe Cloudkey do
     it "should provide an authenticate method on Cloudkey" do
       Cloudkey.should respond_to :authenticate
     end
-    
+
     it "should count medias" do
-      pending
-      @cloudkey = Cloudkey.authenticate "foo", "bar"
-      @cloudkey.media.count
+      VCR.use_cassette('test', :record => :once) do
+        @cloudkey = Cloudkey.authenticate Credentials::USER_ID, Credentials::KEY
+        @cloudkey.media.count.should == 1
+      end
     end
-    
+
     it "should accept an optional proxy" do
       @proxy = "http://my.awesome.proxy.com:3128"
       Cloudkey.authenticate("foo", "bar", :proxy => @proxy).proxy.should == @proxy
     end
-    
+
     it "should accept an optional base_url" do
       @base_url = "http://different.api.dmcloud.net"
       Cloudkey.authenticate("foo", "bar", :base_url => @base_url).base_url.should == @base_url
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,14 @@
+require 'vcr'
 require 'cloudkey'
+require 'credentials' # used to record cassettes
+
+VCR.config do |c|
+  c.cassette_library_dir = "spec/cassettes"
+  c.stub_with :webmock
+  #c.default_cassette_options = {:record => :once}
+end
+
+RSpec.configure do |config|
+  config.extend VCR::RSpec::Macros
+end
+

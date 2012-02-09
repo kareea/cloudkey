8dea3784e55641b0d3ce3b8702c9e46b6b1ad88e
Jean-Hadrien Chabran <jh@chabran.fr>
2012-02-09 15:33:31 +0100

    Wire VCR into our specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,13 @@
+require 'rubygems'
+require 'bundler/setup'
 require 'cloudkey'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.hook_into :webmock
+end
+
+RSpec.configure do |config|
+  config.extend VCR::RSpec::Macros
+end
